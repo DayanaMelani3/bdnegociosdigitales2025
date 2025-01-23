88a6152f0cc1f5c944d6ff467f70eedd5cd1089d
Dayana Melani <dayanamelanialvarezcortes@gmail.com>
2025-01-23 17:46:29 -0600

    Logro: ejercicioterminado-sql-ldd

--- Unidad1/Creación base de datos.sql
+++ Unidad1/Creación base de datos.sql
@@ -42,9 +42,36 @@ values ('Panaderia', 7),
 insert into categoria
 values(10, 'panaderia');
 
-
-
 select * from categoria;
 
+create table producto1(
+productoid int not null,
+nombreProducto varchar(20) not null,
+descripcion varchar(80),
+precio decimal(10,2) not null,
+existencia int not null,
+categoriaid int null,
+constraint pk_producto1
+primary key (productoid),
+constraint unico_descripcion
+unique(nombreProducto),
+constraint chk_precio
+check (precio>0.0 and precio<=1000),
+constraint chk_existencia
+check(existencia>0 and existencia<=200),
+constraint fk_categoria_producto1
+foreign key (categoriaid)
+references categoria(categoriaid)
+);
+
+
+insert into producto1
+values (1,'Miguelito','Dulce sano para la lombriz', 34.5, 45, 5);
+
+insert into producto1
+values (2,'tupsi pop','Dulce sano para la lombriz', -34.5, 45, 5);
+
+insert into producto1
+values 
 
 );
